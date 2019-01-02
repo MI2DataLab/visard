14af24788a95db34d55612e1b51d3b92ed712a5f
Agnieszka Ciepielewska <agnieszkac27@gmail.com>
2019-01-02 14:06:12 +0100

    form for data upload

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import upload_data
+
+urlpatterns = [
+    path('', upload_data, name='upload_data'),
+]
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Path
 
-# Create your views here.
+
+def upload_data(request):
+    if request.method == 'POST':
+        data_dir = request.POST.get('data_dir')
+        print(data_dir)
+        Path.objects.get_or_create(path=data_dir)
+        return plots(request, data_dir)
+    else:
+        return render(request, 'core/upload_data.html', {})
+
+
+def data_for_plots(data_dir):
+    pass
+
+
+def plots(request, data_dir):
+    data = data_for_plots(data_dir)
+    return render(request, 'core/plots.html', {data})
