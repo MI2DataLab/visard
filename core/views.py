79d534d72a2f66253a5f57837ced33f80fa56ddb
Agnieszka Ciepielewska <agnieszkac27@gmail.com>
2019-01-04 10:28:05 +0100

    version with uploading data without api

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+import itertools
+
+from django.shortcuts import render, get_object_or_404
+import pandas as pd
+
 from .models import Path
 
 
@@ -6,16 +10,32 @@ def upload_data(request):
     if request.method == 'POST':
         data_dir = request.POST.get('data_dir')
         print(data_dir)
-        Path.objects.get_or_create(path=data_dir)
-        return plots(request, data_dir)
+        path = Path.objects.get_or_create(path=data_dir)
+        return plots(request, path.pk)
     else:
         return render(request, 'core/upload_data.html', {})
 
 
 def data_for_plots(data_dir):
-    pass
+    data = pd.read_csv(data_dir)
+    sorted_cols = sort_cols(data)
+    sorted_pairs = sort_pairs(data)
+    return data, sorted_cols, sorted_pairs
 
 
-def plots(request, data_dir):
-    data = data_for_plots(data_dir)
-    return render(request, 'core/plots.html', {data})
+def sort_cols(data):
+    return data.columns
+
+
+def sort_pairs(data):
+    return list(itertools.combinations(data.columns, 2))
+
+
+def plots(request, path_id):
+    path = get_object_or_404(Path, pk=path_id)
+    data, sorted_cols, sorted_pairs = data_for_plots(path.path)
+    return render(
+        request,
+        'core/plots.html',
+        {'data': data, 'sorted_cols': sorted_cols, 'sorted_pairs': sorted_pairs},
+    )
